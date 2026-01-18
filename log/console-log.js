fe246a02450bcf6654abe3221706f08f3012a6e4
mern-miraz <w3miraj@gmail.com>
2026-01-18 17:08:04 +0600

    practice console log

---
+++ log/console-log.js
@@ -0,0 +1,17 @@
+// Variable
+const age = 25;
+const booksToRead = 15;
+const weekUpTime = 6;
+const wastedTime = 3;
+const borrow = 50000;
+const goals = 7;
+const pillow = 5;
+
+// Log Output
+console.log(age);
+console.log(booksToRead);
+console.log(weekUpTime);
+console.log(wastedTime);
+console.log(borrow);
+console.log(goals);
+console.log(pillow);
