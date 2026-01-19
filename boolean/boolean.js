74ea1b4567eab11dff8979a6a56f5d3b2d1c4b41
mern-miraz <w3miraj@gmail.com>
2026-01-19 13:49:01 +0600

    Boolean practice done

---
+++ boolean/boolean.js
@@ -0,0 +1,19 @@
+const areYouHappy = false;
+const isMessageSent = false;
+const celebrating = true;
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+const isStudyDone = false;
+const isPhoneCharged = true;
+const isDinnerDone = false;
+
+console.log(areYouHappy);
+console.log(isMessageSent);
+console.log(celebrating);
+console.log(isHappy);
+console.log(hasMoney);
+console.log(isHungry);
+console.log(isStudyDone);
+console.log(isPhoneCharged);
+console.log(isDinnerDone);
