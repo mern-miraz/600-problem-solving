d524a75cbc0ff7f5fc86ec2fae1b73ed9bdedcfe
mern-miraz <w3miraj@gmail.com>
2026-01-19 13:26:20 +0600

    write string

---
+++ String/string.js
@@ -0,0 +1,15 @@
+const myName = "Md Mirazul Islam";
+const address = "Rajshahi";
+const friendsCollege = "Rajshahi Polytechnic Institute";
+const fan = "Tamanna Bhatia";
+const movieOfTheYear = "Saiyara";
+const hobby = "Traveler";
+const color = "Pink";
+
+console.log(myName);
+console.log(address);
+console.log(friendsCollege);
+console.log(fan);
+console.log(movieOfTheYear);
+console.log(hobby);
+console.log(color);
