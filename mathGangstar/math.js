abbd2384365f45ca1be188be98f89db6c1fe1522
mern-miraz <w3miraj@gmail.com>
2026-01-19 15:35:07 +0600

    math practice done

---
+++ mathGangstar/math.js
@@ -0,0 +1,65 @@
+// Number 1 Practice
+const income = 100;
+const expense = 40;
+const remainingBalance = income - expense;
+console.log(remainingBalance);
+
+// Number 2 Practice
+const pencil = 10;
+const everyTimeUse = 2;
+const totalUse = pencil / everyTimeUse;
+console.log(totalUse);
+
+// Number 3 Practice
+const haveMoney = 60;
+const usageMoney = 15;
+const totalUseableMoney = haveMoney / usageMoney;
+console.log(`Total usable time  ${totalUseableMoney}`);
+
+// Number 4 Practice
+const hello = "Hello";
+const World = "World";
+console.log(hello + World);
+
+// Number 5 Practice
+const riceKg = 153;
+const lentilsKg = 261;
+const totalKg = riceKg + lentilsKg;
+console.log(totalKg);
+
+// Number 6 Practice
+const haveMoneyToMe = 500;
+const everyTimeUsed = 75;
+const totalUseTime = haveMoneyToMe / everyTimeUsed;
+const remainingMoney = haveMoneyToMe % everyTimeUsed;
+console.log(totalUseTime);
+console.log(remainingMoney);
+
+// Number 7 Practice
+const dividedBy = 8;
+const dividedTo = 3;
+const result = dividedBy / dividedTo;
+const remainingResult = dividedBy % dividedTo;
+console.log(result);
+console.log(remainingResult);
+
+// Number 8 Practice
+const againDivided = 50;
+const againDividedTo = 9;
+const remainder = againDivided % againDividedTo;
+console.log(remainder);
+
+// Number 9 Practice
+const countryFirst = "Bangla";
+const countryLast = "desh";
+const country = countryFirst + countryLast;
+console.log(country);
+
+// Number 10 Practice
+const singleDay = 4;
+const totalRice = 480;
+const totalDay = totalRice / singleDay;
+const perMonth = 30;
+const totalMonth = totalDay / perMonth;
+console.log(totalDay);
+console.log(totalMonth);
