ff06b61ec9e37e7420206c8f99a9c5f0e0379529
mern-miraz <w3miraj@gmail.com>
2026-01-21 12:12:13 +0600

    name practice is done

---
+++ data_type/name.js
@@ -0,0 +1,24 @@
+const "favFood" = "biryani";
+
+const city = "chittagong";
+const City = "Dhaka";
+console.log(city);
+console.log(City);
+
+const userName = "Johan";
+const username = "jovan";
+console.log(userName);
+console.log(username);
+
+const myAddress = "rajshahi";
+console.log(myAddress);
+
+const super = 12;
+console.log(super);
+
+const class = 12;
+console.log(class)
+
+
+We need to move the number '2' to the beginning of the variable
+const 2ndPosition = "Miraz"
