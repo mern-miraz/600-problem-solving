86f32541be89f4a838e3b153d972c6d3b1270296
mern-miraz <w3miraj@gmail.com>
2026-01-21 10:34:47 +0600

    let practice complete

---
+++ data_type/let.js
@@ -0,0 +1,28 @@
+let age = 25;
+age = 24;
+console.log(`My agg is ${age}`);
+
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(`my pocket money now ${pocketMoney}`);
+
+let studyTime = 4;
+studyTime = 1.5;
+console.log(`Today study time ${studyTime} hours`);
+
+let friendsCount = 3;
+friendsCount = 1;
+console.log(`Now we are stay ${friendsCount} friends`);
+
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(`After one month my phone price is ${phonePrice}`);
+
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+console.log(`have ${chairsCount} in our class room`);
+
+let playTime = 3;
+playTime = 0;
+console.log(`today I'm playing ${playTime} hours`);
