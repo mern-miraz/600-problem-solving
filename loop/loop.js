d97ad9f0da8305ec2b965450647382554ec3dae1
mern-miraz <w3miraj@gmail.com>
2026-01-26 09:12:00 +0600

    complete for loop practice

---
+++ loop/loop.js
@@ -0,0 +1,36 @@
+const subject = ["math", "english", "bangla", "psychology", "chemistry"];
+for (const sub of subject) {
+  console.log(sub);
+}
+
+const favFood = ["biriyani", "beef", "juice", "egg", "khejur"];
+for (const fav of favFood) {
+  console.log(fav);
+}
+
+const familyMember = [1999, 1962, 1967, 1995, 1993, 2021, 2004];
+for (const fam of familyMember) {
+  console.log(fam);
+}
+
+const cricketTeam = [
+  "tamim",
+  "sakib",
+  "shanto",
+  "nesham",
+  "binura",
+  "lamichane",
+  "washem",
+  "williamson",
+  "mushfiqur",
+  "saqlain",
+  "hasan murad",
+];
+for (const cric of cricketTeam) {
+  console.log(cric);
+}
+
+const examDate = [12, 15, 18, 20, 21, 23, 25, 28];
+for (const exam of examDate) {
+  console.log(exam);
+}
