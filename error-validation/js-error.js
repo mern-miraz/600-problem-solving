5365d01c672c91990a69733bdcdd7dbe9b086410
mern-miraz <w3miraj@gmail.com>
2026-02-19 11:22:04 +0600

    Complete practice on JavaScript error types

---
+++ error-validation/js-error.js
@@ -0,0 +1,15 @@
+// =====================
+// Practice: JavaScript Error Types
+// =====================
+
+// Practice 1: SyntaxError
+const syntaxError =
+  "If we forget to add some symbols after finishing the code, such as a semicolon, comma, or bracket, then JavaScript returns a SyntaxError.";
+
+// Practice 2: ReferenceError
+const referenceError =
+  "When we use a variable that has not been declared, JavaScript gives us a ReferenceError.";
+
+// Practice 3: Difference between SyntaxError and TypeError
+const diffSyntaxAndTypeError =
+  "A SyntaxError occurs when something is missing in the code, such as a comma, semicolon, bracket, or when we use a JavaScript reserved keyword incorrectly. A TypeError occurs when an operation is performed on the wrong data type. For example, if a variable contains a number and we try to use toUpperCase() on it, JavaScript throws a TypeError because that method works only on strings.";
