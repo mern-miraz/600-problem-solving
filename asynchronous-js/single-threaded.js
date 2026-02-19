b7b513dd964f0b3f2b6d3ddc66ecfe907a0f2a54
mern-miraz <w3miraj@gmail.com>
2026-02-19 12:51:16 +0600

    docs: add definitions for synchronous execution and single-threaded nature

---
+++ asynchronous-js/single-threaded.js
@@ -0,0 +1,7 @@
+// Practice 1: What happens after calling a function in synchronous code?
+const synchronous =
+  "In synchronous code, functions are executed in a top-to-bottom serial order. When a function is called, the engine waits for it to finish completely before moving on to the next line of code. For example, if you print numbers 1 to 5 and call a function after number 2, the engine will execute that function fully before printing number 3.";
+
+// Practice 2: Is JavaScript single-threaded?
+const singleThreaded =
+  "Yes, JavaScript is a single-threaded language. This means it has only one call stack and can execute only one command at a time in a single sequence.";
