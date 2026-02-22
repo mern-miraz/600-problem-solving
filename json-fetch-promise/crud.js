809468e5d26eaab75fffe23e379bed8bc3f3598c
mern-miraz <w3miraj@gmail.com>
2026-02-22 09:42:56 +0600

    Add HTTP methods and CRUD practice notes

---
+++ json-fetch-promise/crud.js
@@ -0,0 +1,33 @@
+// =======================================
+// Practice 1: PUT vs PATCH (Profile status update)
+// =======================================
+const singleOrMarried =
+  "When you want to change only a specific part of a profile, such as single or married status, you should use PATCH.";
+console.log(singleOrMarried);
+
+// =======================================
+// Practice 2: Add new data to database
+// =======================================
+const addData =
+  "To add new information to a database, the HTTP POST method is used.";
+console.log(addData);
+
+// =======================================
+// Practice 3: Watching a video on a website
+// =======================================
+const watch =
+  "When you watch a video on a website, it uses a GET operation to fetch data from the server.";
+console.log(watch);
+
+// =======================================
+// Practice 4: CRUD operations
+// =======================================
+const crud = "Create, Read, Update, Delete";
+console.log(crud);
+
+// =======================================
+// Practice 5: HTTP methods and their usage
+// =======================================
+const httpMethod =
+  "GET, POST, PUT, PATCH, DELETE. GET is used to get data from the server. POST is used to add new data. PUT is used to update the entire data. PATCH is used to update a specific part of data. DELETE is used to remove data from the server.";
+console.log(httpMethod);
