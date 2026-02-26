d6bebed09fdfd5f20a71734838a3794c6ca61586
mern-miraz <w3miraj@gmail.com>
2026-02-26 12:08:58 +0600

    practice: understand window object and this keyword in browser

---
+++ js_this_context/confusing-this.js
@@ -0,0 +1,11 @@
+// Practice 1
+const windowExplanation =
+  "In the browser, window is the global object that contains all global variables and functions.";
+
+// Practice 2
+const thisValueInBrowser =
+  "When console.log(this) is run in the browser global scope, it refers to the window object.";
+
+// Practice 3
+const diffArrowAndNormal =
+  "In object methods, normal functions have their own this pointing to the object, but arrow functions do not have their own this and take it from the outer scope.";
