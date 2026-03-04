02054da1ec1501fbf242ba53bdd61525cd584456
mern-miraz <w3miraj@gmail.com>
2026-03-04 09:36:48 +0600

    Complete JS Book

--- js_build-in_object/map-set.js
+++ js_build-in_object/map-set.js
@@ -1,9 +1,13 @@
-// Peactice 1
+// ============================================================
+// 🚀 JAVASCRIPT PRACTICE: SETS (UNIQUE DATA COLLECTIONS)
+// ============================================================
+
+// Practice 1: Creating a Set from an Array to remove duplicates
 const numbers = [1, 2, 2, 3, 4, 4, 5];
 const uniqueNumber = new Set(numbers);
 console.log(uniqueNumber);
 
-// Peactice 2
+// Practice 2: Adding values to a Set
 const newSet = new Set();
 newSet.add(10);
 newSet.add(20);
@@ -11,12 +15,12 @@ newSet.add(10);
 newSet.add(30);
 console.log(newSet);
 
-// Peactice 3
+// Practice 3: Deleting values from a Set
 const set = new Set([10, 20, 30]);
 const deleteSet = set.delete(10);
 console.log(set);
 
-// Peactice 4
+// Practice 4: Converting a Set back to an Array
 const newNumbers = [1, 2, 3, 4, 2, 1, 5, 5];
 const uniqueNumbers = [...new Set(newNumbers)];
 console.log(uniqueNumbers);
